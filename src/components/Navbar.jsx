dd0e63f98f712385b8acf4d6f620be4787b2b641
hebem <hebemilanesio1@gmail.com>
2025-02-19 12:30:59 -0300

    Modificando navbar mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo_amolen.png';
 import './Navbar.css'; // Importa el CSS aquí
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -11,15 +17,21 @@ const Navbar = () => {
           <img src={logo} alt="Amolen Logo" />
         </Link>
       </div>
-      <ul className="navbar-links">
+      <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
         <li><Link to="/">¿Quién soy?</Link></li>
         <li><Link to="/estudios">Estudios</Link></li>
         <li><Link to="/diseños">Diseños</Link></li>
         <li><Link to="/contacto">Contacto</Link></li>
       </ul>
+      <div className="navbar-toggle" onClick={toggleMenu}>
+        <span className="navbar-toggle-icon"></span>
+        <span className="navbar-toggle-icon"></span>
+        <span className="navbar-toggle-icon"></span>
+      </div>
     </nav>
   );
 };
 
 export default Navbar;
+
 
